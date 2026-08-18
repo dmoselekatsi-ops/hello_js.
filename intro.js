efdc937eacdde192c4185cd5b0efe91a2e915177
dmoselekatsi-ops <dmoselekatsi@gmail.com>
2026-08-18 11:34:40 +0200

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,7 @@
+let name = "Dolly";
+let age = 20;
+let favoriteLanguage = "Python";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
